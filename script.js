66eacb557de0c22c8633c4a8e82ffd28d112ab00
Gibson Tan <tysgibson@gmail.com>
2024-02-07 08:00:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Writing the javascript now to make our hamburger icon interactive!
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
